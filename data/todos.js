7d0d3a9fa4bd09d12581eb2f154ae4722988994f
Victor Catao <victorcatao@hotmail.com>
2025-07-20 18:05:49 -0300

    Initial commit

---
+++ data/todos.js
@@ -0,0 +1,102 @@
+export const data = [
+    {
+        "id": 1,
+        "title": "Buy groceries",
+        "completed": true
+    },
+    {
+        "id": 2,
+        "title": "Walk the dog",
+        "completed": false
+    },
+    {
+        "id": 3,
+        "title": "Read a book",
+        "completed": false
+    },
+    {
+        "id": 4,
+        "title": "Do laundry",
+        "completed": true
+    },
+    {
+        "id": 5,
+        "title": "Prepare dinner",
+        "completed": false
+    },
+    {
+        "id": 6,
+        "title": "Clean the house",
+        "completed": true
+    },
+    {
+        "id": 7,
+        "title": "Finish project report",
+        "completed": false
+    },
+    {
+        "id": 8,
+        "title": "Call mom",
+        "completed": true
+    },
+    {
+        "id": 9,
+        "title": "Plan weekend trip",
+        "completed": false
+    },
+    {
+        "id": 10,
+        "title": "Attend yoga class",
+        "completed": true
+    },
+    {
+        "id": 11,
+        "title": "Grocery shopping",
+        "completed": false
+    },
+    {
+        "id": 12,
+        "title": "Visit the dentist",
+        "completed": true
+    },
+    {
+        "id": 13,
+        "title": "Organize workspace",
+        "completed": false
+    },
+    {
+        "id": 14,
+        "title": "Watch a movie",
+        "completed": true
+    },
+    {
+        "id": 15,
+        "title": "Write blog post",
+        "completed": false
+    },
+    {
+        "id": 16,
+        "title": "Attend conference",
+        "completed": true
+    },
+    {
+        "id": 17,
+        "title": "Plan birthday party",
+        "completed": false
+    },
+    {
+        "id": 18,
+        "title": "Update resume",
+        "completed": true
+    },
+    {
+        "id": 19,
+        "title": "Learn a new skill",
+        "completed": false
+    },
+    {
+        "id": 20,
+        "title": "Volunteer at local shelter",
+        "completed": true
+    }
+]
